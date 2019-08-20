04b4256cfa93377d709d4697b837e7eeaf071420
TiagoJava2019 <almeidaschwingel.ll@gmail.com>
2019-08-20 00:36:37 -0300

    Projeto foi criado

---
+++ src/main/java/com/tiagoalmeida/java/SpringbootcrudjavaApplication.java
@@ -0,0 +1,13 @@
+package com.tiagoalmeida.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcrudjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcrudjavaApplication.class, args);
+	}
+
+}
